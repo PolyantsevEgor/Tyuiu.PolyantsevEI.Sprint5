d94788ae26fea2883aa771187b32493d5dc85058
Tyuiu.PolyantsevEI@outlook.com <qreft@mail.ru>
2023-11-19 20:21:19 +0500

    Task2

---
+++ Tyuiu.PolyantsevEI.Sprint5.Task2.V10.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.PolyantsevEI.Sprint5.Task2.V10.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.PolyantsevEI.Sprint5.Task2.V10.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2c9b6a51-6372-4c7f-a73e-c9a2b1b84dcb")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
