83c3b27d962e755843cfeaee9c230531849cb0d5
Tyuiu.PolyantsevEI@outlook.com <qreft@mail.ru>
2023-11-26 23:37:25 +0500

    Fix

--- Tyuiu.PolyantsevEI.Sprint5.Task5.V27.Test/DataServiceTest.cs
+++ Tyuiu.PolyantsevEI.Sprint5.Task5.V27.Test/DataServiceTest.cs
@@ -10,7 +10,7 @@ namespace Tyuiu.PolyantsevEI.Sprint5.Task5.V27.Test
         [TestMethod]
         public void CheckExistsFile()
         {
-            string path = @"C:\DataSprint5\InPutDataFileTask6V1.txt";
+            string path = @"C:\DataSprint5\InPutDataFileTask5V27.txt";
 
             FileInfo fileInfo = new FileInfo(path);
             bool fileExists = fileInfo.Exists;
--- Tyuiu.PolyantsevEI.Sprint5.Task6.V1.Test/DataServiceTest.cs
+++ Tyuiu.PolyantsevEI.Sprint5.Task6.V1.Test/DataServiceTest.cs
@@ -10,7 +10,7 @@ namespace Tyuiu.PolyantsevEI.Sprint5.Task6.V1.Test
         [TestMethod]
         public void CheckExistsFile()
         {
-            string path = @"C:\DataSprint5\InPutDataFileTask5V27.txt";
+            string path = @"C:\DataSprint5\InPutDataFileTask6V1.txt";
 
             FileInfo fileInfo = new FileInfo(path);
             bool fileExists = fileInfo.Exists;
